968485480ffa62d3a18553690012f52228ccd2d5
Alekhya <asri42080@gmail.com>
2023-02-24 15:20:44 +0530

    moving into CI

---
+++ 01-sample/test.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "ec2" {
+  ami = "ami-0a017d8ceb274537d"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-0a6f9986a7f6c10cd"]
+  tags = {
+    Name = "test"
+  }
+
+}
