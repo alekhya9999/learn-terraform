db0041a1e46c47fe8a23f9f960b991ded4cb551b
Alekhya <asri42080@gmail.com>
2023-02-26 07:29:18 +0530

    Terraform learning

--- roboshop-setup/ex2/spotrequest.tf
+++ roboshop-setup/ex2/spotrequest.tf
@@ -2,6 +2,7 @@ resource "aws_spot_instance_request" "spotrequest" {
   ami           = "ami-0a017d8ceb274537d"
   spot_price    = "0.03"
   instance_type = "t3.micro"
+  status = "active"
 
   tags = {
     Name = "spotrequest"
