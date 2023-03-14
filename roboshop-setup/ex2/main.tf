c80930887cfd907171931210e4939a095e262e0a
Alekhya <asri42080@gmail.com>
2023-03-14 22:16:33 +0530

    Terraform learning

--- roboshop-setup/ex2/main.tf
+++ roboshop-setup/ex2/main.tf
@@ -6,10 +6,11 @@ data "aws_ami" "ami" {
 resource "aws_instance" "frontend" {
   for_each = var.instances
   ami                    = data.aws_ami.ami.image_id
+
   instance_type          = each.value["type"]
   vpc_security_group_ids = ["sg-0a6f9986a7f6c10cd"]
     tags    = {
-    name  = each.value["name"]
+    name  = each.value["Name"]
   }
 
 }
