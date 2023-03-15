3fa1a102d5b31706b1a46f95ff5da4beebdcc505
Alekhya <asri42080@gmail.com>
2023-03-15 06:46:29 +0530

    Terraform learning

---
+++ roboshop-setup/ex4/ec2/main.tf
@@ -0,0 +1,25 @@
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+resource "aws_instance" "ec2" {
+  ami                    = data.aws_ami.ami.image_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = ["sg-0a6f9986a7f6c10cd"]
+  tags = {
+    Name = var.component
+  }
+}
+
+resource "aws_route53_record" "record" {
+  zone_id = "Z0842500299XCGXBYVP8R"
+  name    = "${var.component}-dev.ambatis.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.ec2.private_ip]
+}
+
+variable "component" {}
+variable "instance_type" {}
