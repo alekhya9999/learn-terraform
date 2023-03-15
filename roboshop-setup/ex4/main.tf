c16234223173a03410ae3d8b749a745620fbc20c
Alekhya <asri42080@gmail.com>
2023-03-15 06:52:52 +0530

    Terraform learning

--- roboshop-setup/ex4/main.tf
+++ roboshop-setup/ex4/main.tf
@@ -2,7 +2,7 @@ module "ec2" {
   for_each = var.instances
   source = "./ec2"
   component = each.value["name"]
-  instance_type = each.value["type "]
+  instance_type = each.value["type"]
 }
 
 variable "instances" {
